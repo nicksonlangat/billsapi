2f2454924cea49820fb6077b2a35aa9af74038b6
nicksonlangat <nicksonlangat95@gmail.com>
2022-05-19 15:26:58 +0300

    env variables

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+from decouple import config
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_DETAILS = config("prod_string") 
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
